b8af0928f4418d54b44493fc1d515285c3e7e67c
ZiQi-Jiang <jzq2369550710@gmail.com>
2022-07-10 16:36:51 +0800

    Add CS131

---
+++ CS131/README.md
@@ -0,0 +1,4 @@
+Stanford CS131-2020 fall: traditional 2D Computer Vision.
+
+NOTE:This course does not cover many content about 3D CV, and you have to take Stanford CS231A if you want to learn that
+
---
+++ CS131/hw4_release/segmentation.py
@@ -0,0 +1,314 @@
+"""
+CS131 - Computer Vision: Foundations and Applications
+Assignment 4
+Author: Donsuk Lee (donlee90@stanford.edu)
+Date created: 09/2017
+Last modified: 10/9/2020
+Python Version: 3.5+
+"""
+
+from os import pardir
+import numpy as np
+import random
+from scipy.spatial.distance import squareform, pdist, cdist
+from skimage.util import img_as_float
+
+### Clustering Methods
+def kmeans(features, k, num_iters=100):
+    """ Use kmeans algorithm to group features into k clusters.
+
+    K-Means algorithm can be broken down into following steps:
+        1. Randomly initialize cluster centers
+        2. Assign each point to the closest center
+        3. Compute new center of each cluster
+        4. Stop if cluster assignments did not change
+        5. Go to step 2
+
+    Args:
+        features - Array of N features vectors. Each row represents a feature
+            vector.
+        k - Number of clusters to form.
+        num_iters - Maximum number of iterations the algorithm will run.
+
+    Returns:
+        assignments - Array representing cluster assignment of each point.
+            (e.g. i-th point is assigned to cluster assignments[i])
+    """
+
+    N, D = features.shape
+
+    assert N >= k, 'Number of clusters cannot be greater than number of points'
+
+    # Randomly initalize cluster centers
+    idxs = np.random.choice(N, size=k, replace=False)
+    centers = features[idxs]
+    assignments = np.zeros(N, dtype=np.uint32)
+
+    for n in range(num_iters):
+        ### YOUR CODE HERE
+        pass
+        for i in range(N):
+            assignments[i] =  np.argmin(np.sum(np.square(centers - features[i]),axis=1))
+        tmp = centers.copy()
+        for i in range(k):
+            if np.any(assignments==i):
+                centers[i] = np.mean(features[assignments==i],axis=0)
+        if np.allclose(tmp,centers):
+            break
+        ### END YOUR CODE
+
+    return assignments
+
+def kmeans_fast(features, k, num_iters=100):
+    """ Use kmeans algorithm to group features into k clusters.
+
+    This function makes use of numpy functions and broadcasting to speed up the
+    first part(cluster assignment) of kmeans algorithm.
+
+    Hints
+    - You may find cdist (imported from scipy.spatial.distance) and np.argmin useful
+
+    Args:
+        features - Array of N features vectors. Each row represents a feature
+            vector.
+        k - Number of clusters to form.
+        num_iters - Maximum number of iterations the algorithm will run.
+
+    Returns:
+        assignments - Array representing cluster assignment of each point.
+            (e.g. i-th point is assigned to cluster assignments[i])
+    """
+
+    N, D = features.shape
+
+    assert N >= k, 'Number of clusters cannot be greater than number of points'
+
+    # Randomly initalize cluster centers
+    idxs = np.random.choice(N, size=k, replace=False)
+    centers = features[idxs]
+    assignments = np.zeros(N, dtype=np.uint32)
+    for n in range(num_iters):
+        ### YOUR CODE HERE
+        pass
+        old_centers = centers.copy()
+        dist = cdist(features,centers) # N X K
+        assignments = np.argmin(dist,axis=1)
+        for i in range(k):
+            if np.any(assignments==i):#很重要，不然空集合取平均会让center变成nan
+                centers[i] = np.mean(features[assignments==i],axis=0)
+        # import pdb
+        # pdb.set_trace()
+        if np.allclose(centers,old_centers):
+            break
+        ### END YOUR CODE
+
+    return assignments
+
+
+
+def hierarchical_clustering(features, k):
+    """ Run the hierarchical agglomerative clustering algorithm.
+
+    The algorithm is conceptually simple:
+
+    Assign each point to its own cluster
+    While the number of clusters is greater than k:
+        Compute the distance between all pairs of clusters
+        Merge the pair of clusters that are closest to each other
+
+    We will use Euclidean distance to define distance between clusters.
+
+    Recomputing the centroids of all clusters and the distances between all
+    pairs of centroids at each step of the loop would be very slow. Thankfully
+    most of the distances and centroids remain the same in successive
+    iterations of the outer loop; therefore we can speed up the computation by
+    only recomputing the centroid and distances for the new merged cluster.
+
+    Even with this trick, this algorithm will consume a lot of memory and run
+    very slowly when clustering large set of points. In practice, you probably
+    do not want to use this algorithm to cluster more than 10,000 points.
+    
+    Hints
+    - You may find pdist (imported from scipy.spatial.distance) useful
+
+    Args:
+        features - Array of N features vectors. Each row represents a feature
+            vector.
+        k - Number of clusters to form.
+
+    Returns:
+        assignments - Array representing cluster assignment of each point.
+            (e.g. i-th point is assigned to cluster assignments[i])
+    """
+
+
+
+    N, D = features.shape
+
+    assert N >= k, 'Number of clusters cannot be greater than number of points'
+
+    # Assign each point to its own cluster
+    assignments = np.arange(N, dtype=np.uint32)
+    centers = np.copy(features)
+    n_clusters = N
+    place_holder = np.array([1000 for i in range(D)])
+    while n_clusters > k:
+        ### YOUR CODE HERE
+        pass
+        dist_vector = pdist(centers)
+        dist_vector[dist_vector==0] = float("inf")
+        dist_matrix = squareform(dist_vector)
+        pair = np.where(dist_matrix==np.min(dist_vector))
+        new_index,old_index = np.min(assignments[pair[0][0]]),np.max(assignments[pair[1][0]])
+        import pdb
+        # pdb.set_trace()
+        assignments[assignments==old_index] = new_index
+        new_center = np.mean(features[assignments==new_index],axis=0)
+        centers[assignments==old_index ] = new_center
+        centers[assignments==new_index ] = new_center#不删除是为了保持序号的对应关系
+        n_clusters-=1
+        if(len(set(assignments))!=n_clusters):
+            pdb.set_trace()
+    index = list(set(assignments))
+    # pdb.set_trace()
+    for i in range(k):
+        assignments[assignments==index[i]] = i
+    # import pdb
+    # pdb.set_trace()
+        ### END YOUR CODE
+
+    return assignments
+
+
+### Pixel-Level Features
+def color_features(img):
+    """ Represents a pixel by its color.
+
+    Args:
+        img - array of shape (H, W, C)
+
+    Returns:
+        features - array of (H * W, C)
+    """
+    H, W, C = img.shape
+    img = img_as_float(img)
+    features = np.zeros((H*W, C))
+
+    ### YOUR CODE HERE
+    pass
+    features = img.reshape((H*W,C))
+    ### END YOUR CODE
+
+    return features
+
+def color_position_features(img):
+    """ Represents a pixel by its color and position.
+
+    Combine pixel's RGB value and xy coordinates into a feature vector.
+    i.e. for a pixel of color (r, g, b) located at position (x, y) in the
+    image. its feature vector would be (r, g, b, x, y).
+
+    Don't forget to normalize features.
+
+    Hints
+    - You may find np.mgrid and np.dstack useful
+    - You may use np.mean and np.std
+
+    Args:
+        img - array of shape (H, W, C)
+
+    Returns:
+        features - array of (H * W, C+2)
+    """
+    H, W, C = img.shape
+    color = img_as_float(img)
+    features = np.zeros((H*W, C+2))
+
+    ### YOUR CODE HERE
+    pass
+    position = np.stack(np.mgrid[:H,:W],axis=-1).reshape((H*W,2))# equals to np.dstack(np.mgrid[:H,:W])
+    import pdb
+    # pdb.set_trace()
+
+    features[:,:C] = color.reshape(H*W,C)
+    features[:,C-1:-1] =  position
+
+    mu = np.mean(features,axis=0)
+    sigma = np.std(features,axis=0)+1e-8
+    features = (features-mu)/sigma
+    # pdb.set_trace()
+
+    ### END YOUR CODE
+
+    return features
+
+def my_features(img):
+    """ Implement your own features
+
+    Args:
+        img - array of shape (H, W, C)
+
+    Returns:
+        features - array of (H * W, C)
+    """
+    features = None
+    ### YOUR CODE HERE
+    pass
+
+    ### END YOUR CODE
+    return features
+
+
+### Quantitative Evaluation
+def compute_accuracy(mask_gt, mask):
+    """ Compute the pixel-wise accuracy of a foreground-background segmentation
+        given a ground truth segmentation.
+
+    Args:
+        mask_gt - The ground truth foreground-background segmentation. A
+            logical of size H x W where mask_gt[y, x] is 1 if and only if
+            pixel (y, x) of the original image was part of the foreground.
+        mask - The estimated foreground-background segmentation. A logical
+            array of the same size and format as mask_gt.
+
+    Returns:
+        accuracy - The fraction of pixels where mask_gt and mask agree. A
+            bigger number is better, where 1.0 indicates a perfect segmentation.
+    """
+
+    accuracy = None
+    ### YOUR CODE HERE
+    pass
+    ### END YOUR CODE
+
+    return accuracy
+
+def evaluate_segmentation(mask_gt, segments):
+    """ Compare the estimated segmentation with the ground truth.
+
+    Note that 'mask_gt' is a binary mask, while 'segments' contain k segments.
+    This function compares each segment in 'segments' with the ground truth and
+    outputs the accuracy of the best segment.
+
+    Args:
+        mask_gt - The ground truth foreground-background segmentation. A
+            logical of size H x W where mask_gt[y, x] is 1 if and only if
+            pixel (y, x) of the original image was part of the foreground.
+        segments - An array of the same size as mask_gt. The value of a pixel
+            indicates the segment it belongs.
+
+    Returns:
+        best_accuracy - Accuracy of the best performing segment.
+            0 <= accuracy <= 1, where 1.0 indicates a perfect segmentation.
+    """
+
+    num_segments = np.max(segments) + 1
+    best_accuracy = 0
+
+    # Compare each segment in 'segments' with the ground truth
+    for i in range(num_segments):
+        mask = (segments == i).astype(int)
+        accuracy = compute_accuracy(mask_gt, mask)
+        best_accuracy = max(accuracy, best_accuracy)
+
+    return best_accuracy
